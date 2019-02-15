09b04bf4c0a7ecbb6cad91e1e835765dd34c56a4
Victor Mendele <victor.mendele68@gmail.com>
2019-02-15 15:10:47 +0100

    comments

--- src/app.js
+++ src/app.js
@@ -20,29 +20,41 @@ app.use(fileUpload())
 
 app.post('/character-checker', (req, res) => {
     
+    // get all files from this request
     const { files = {} } = req
+
+    // get the file named 'file'
     const { file = {} } = files || {}
 
+    // if it is not defined then we return a response
     if (file === undefined || file === null || Object.keys(file) == 0) {
         return res.json({
             message: 'You need to upload a file'
         })
     }
 
+    // will store all character issues found inside the file
     let issues = []
 
+    // instatiate a new character checker stream
     const checker = characterChecker()
 
+    // instantiate the stream which will carry the file buffer
     const stream = new Readable()
     stream.push(file.data)
+    // end of stream
     stream.push(null)
+
 
     stream
         .pipe(checker)
+        // checker stream returns objects containing information about the issue
+        // we just add this object to an array
         .on('data', o => { 
             issues = [...issues, ...[o]] 
             console.log(o)
         })
+        // when every issues are found we returns the array as a JSON.
         .on('end', () => {
             res.json({
                 message: 'Ok',
