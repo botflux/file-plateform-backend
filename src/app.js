d29cfa768452a8c37508f55f74836d47d2719b43
Victor Mendele <victor.mendele68@gmail.com>
2019-03-07 10:21:09 +0100

    fix: comments

--- src/app.js
+++ src/app.js
@@ -7,24 +7,30 @@ const CSVToXMLRoutes = require('./routes/csv-to-xml')
 
 const app = express()
 
+// middleware to add cors headers
 app.use(cors())
+
+// middleware to parse json bodies
 app.use(bodyParser.json({
     limit: '50mb'
 }))
+
+// middleware  to parse urlencoded bodies
 app.use(bodyParser.urlencoded({ 
     extended: true, 
     limit: '50mb' 
 }))
+
+// handle file upload
 app.use(fileUpload())
 
+// expose the /result folder
 app.use(express.static('/result'))
 
 app.post('/character-checker', characterCheckerRoutes.index)
 
 app.post('/csv-to-xml/get-headers', CSVToXMLRoutes.getHeaders)
-
 app.get('/csv-to-xml/converted/:name', CSVToXMLRoutes.download)
-
 app.post('/csv-to-xml', CSVToXMLRoutes.index)
 
 module.exports = app
