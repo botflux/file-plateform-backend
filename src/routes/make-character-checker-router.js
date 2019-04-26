19f2fbd16bd70775d02ea4af6ac1e8ec448df3d9
Victor Mendele <victor.mendele68@gmail.com>
2019-04-26 16:44:19 +0200

    chore: refactor character checker router

--- src/routes/make-character-checker-router.js
+++ src/routes/make-character-checker-router.js
@@ -17,54 +17,58 @@ const makeFilesExistsMiddleware = require('../middleware/make-files-exists-middl
 const makeCharacterCheckerRouter = ({ settings }) => {    
     const router = new Router()
 
-    router.use('/', [
+    /**
+     * This router need authentication
+     */
+    router.use([
         makeJwtMiddleware(settings.appSecret, settings.tokenHeader),
-        // makeAuthorizarionMiddleware(['ROLE_USER', 'ROLE_ADMIN']),
-        ...makeAuthorizationMiddleware(['ROLE_USER', 'ROLE_ADMIN']),
-        makeFilesExistsMiddleware([ 'file' ]),
-        makeFileEncodingMiddleware([ 'file' ]),
+        ...makeAuthorizationMiddleware(['ROLE_USER', 'ROLE_ADMIN'])
     ])
 
-    router.post('/', (req, res) => {
+    router.post('/', [
+        makeFilesExistsMiddleware([ 'file' ]),
+        makeFileEncodingMiddleware([ 'file' ]),
+        (req, res) => {
         
-        // get all files from this request
-        const { files = {} } = req
-
-        // get the file named 'file'
-        const { file = {} } = files
-
-        // will store all character issues found inside the file
-        let issues = []
-
-        // instatiate a new character checker stream
-        const checker = characterChecker()
-
-        // instantiate the stream which will carry the file buffer
-        const stream = new Readable()
-        stream.push(file.data)
-        // end of stream
-        stream.push(null)
-
-        let id = 0
-
-        stream
-            .pipe(checker)
-            // checker stream returns objects containing information about the issue
-            // we just add this object to an array
-            .on('data', o => { 
-                issues = [ 
-                    ...issues, 
-                    { ...o, id: id++ } 
-                ] 
-            })
-            // when every issues are found we returns the array as a JSON.
-            .on('end', () => {
-                res.json({
-                    error: false,
-                    result: issues
+            // get all files from this request
+            const { files = {} } = req
+    
+            // get the file named 'file'
+            const { file = {} } = files
+    
+            // will store all character issues found inside the file
+            let issues = []
+    
+            // instatiate a new character checker stream
+            const checker = characterChecker()
+    
+            // instantiate the stream which will carry the file buffer
+            const stream = new Readable()
+            stream.push(file.data)
+            // end of stream
+            stream.push(null)
+    
+            let id = 0
+    
+            stream
+                .pipe(checker)
+                // checker stream returns objects containing information about the issue
+                // we just add this object to an array
+                .on('data', o => { 
+                    issues = [ 
+                        ...issues, 
+                        { ...o, id: id++ } 
+                    ] 
                 })
-            })
-    })
+                // when every issues are found we returns the array as a JSON.
+                .on('end', () => {
+                    res.json({
+                        error: false,
+                        result: issues
+                    })
+                })
+        }
+    ])
 
     return router
 }
