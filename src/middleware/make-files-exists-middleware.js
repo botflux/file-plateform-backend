129e81a2165482eaefb3848c32f4c960e1455ee1
Victor Mendele <victor.mendele68@gmail.com>
2019-04-26 15:49:13 +0200

    chore: change file exists errors

--- src/middleware/make-files-exists-middleware.js
+++ src/middleware/make-files-exists-middleware.js
@@ -1,3 +1,5 @@
+const HTTPError = require('../error/http-error')
+
 /**
  * Make a file verification middleware that tests if each item of 
  * filename list is present is the request
@@ -10,16 +12,17 @@ const makeFileExistsMiddleware = filenames => (req, res, next) => {
     let missingFiles = []
     
     filenames.forEach(filename => {
-        if (!(filename in files)) {
+        if (!(filename in (files || {}))) {
             missing = true
             missingFiles = [...missingFiles, filename]
         }
     })
 
     if (missing) {
-        res
-            .status(400)    
-            .send(`The file ${missingFiles.reduce((p, n, i) => p + (i !== 0 ? ',' : '') + n, '')} is missing`)
+        next(new HTTPError(400, 'File is missing'))
+        // res
+        //     .status(400)    
+        //     .send(`The file ${missingFiles.reduce((p, n, i) => p + (i !== 0 ? ',' : '') + n, '')} is missing`)
     } else {
         next()
     }
